a69c7599ead8507cf474cb6a48126ffac0eb2480
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-07-06 21:15:54 +0500

    updated

--- NamazTimeLeft/app/src/main/java/com/example/myapplication/MainActivity.java
+++ NamazTimeLeft/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -263,7 +263,7 @@ public class MainActivity extends AppCompatActivity{
         t_Isha.setText(       spaces_in_leading + "Isha ------------------: " + results[9] + delim + time_int_to_OK_time(results[16]));
     }
     public Runnable DO_THE_JOB(){
-        int    current_time = current_time_(); // 781
+        int current_time = current_time_(); // 781
         String system_date  = current_date();  // 6-29
         String[] ALL_DATA = get_long_list();
 
